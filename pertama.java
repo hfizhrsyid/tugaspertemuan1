bc8c775317135ea368ca4c287548c39bcf2db037
Hafizh Rasyid Ridha <hfizhrsyid21@gmail.com>
2025-02-11 15:17:48 +0700

    Add files via upload

---
+++ pertama.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugasPertemuan1;
+
+/**
+ *
+ * @author User
+ */
+public class pertama {
+    public static void main(String[] args){
+        //Print teks
+        System.out.println("Hello World! Nama saya Hafizh Rasyid Ridha dan NIM saya 2309549. Saya Senang Belajar Java");
+    }
+}
